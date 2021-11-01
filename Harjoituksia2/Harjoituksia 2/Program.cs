47911a4ea9b3575800911ac2e1fa533cf30a4081
unknown <sanna.kunnasluoto@edu.keuda.fi>
2021-11-01 10:47:15 +0200

    tehtävä7

--- Harjoituksia2/Harjoituksia 2/Program.cs
+++ Harjoituksia2/Harjoituksia 2/Program.cs
@@ -148,146 +148,72 @@ namespace Harjoituksia_2
                     break;
             }
         }
-            static void Tehtävä6()
+        static void Tehtävä6()
+        {
+            Console.WriteLine("Syötä bonuspisteesi väliltä 1-9: ");
+            int pisteet = int.Parse(Console.ReadLine());
+            switch (pisteet)
             {
-                Console.WriteLine("Syötä bonuspisteesi väliltä 1-9: ");
-                int pisteet = int.Parse(Console.ReadLine());
-                switch (pisteet)
-                {
-                    case 1:
-                    case 2:
-                    case 3:
-                        Console.WriteLine("Pisteesi: " + pisteet);
-                        Console.WriteLine("Bonuspisteesi kerroin: 10");
-                        Console.WriteLine("Yhteensä: " + (pisteet * 10));
-                        break;
-                    case 4:
-                    case 5:
-                    case 6:
-                        Console.WriteLine("Pisteesi: " + pisteet);
-                        Console.WriteLine("Bonuspisteesi kerroin: 100");
-                        Console.WriteLine("Yhteensä: " + (pisteet * 100));
-                        break;
-                    case 7:
-                    case 8:
-                    case 9:
-                        Console.WriteLine("Pisteesi: " + pisteet);
-                        Console.WriteLine("Bonuspisteesi kerroin: 1000");
-                        Console.WriteLine("Yhteensä: " + (pisteet * 1000));
-                        break;
-                    default:
-                        Console.WriteLine("Tarkista syöttämäsi luku!");
-                        break;
-                }
-            }
-                /*
-                static void Main)
-                {
-                    
-                Console.WriteLine("Anna luku 0-999: ");
-                int nro = int.Parse(Console.ReadLine());
-                string muunnos = "";
-                switch (nro)
-                {
-                        case 0:
-                        Console.WriteLine("nolla");
-                        break;
-                        case 1:
-                        Console.WriteLine("yksi");
-                        break;
-                        case 2:
-                        Console.WriteLine("kaksi");
-                        break;
-                        case 3:
-                        Console.WriteLine("kolme");
-                        break;
-                        case 4:
-                        Console.WriteLine("neljä");
-                        break;
-                        case 5:
-                        Console.WriteLine("viisi");
-                        break;
-                        case 6:
-                        Console.WriteLine("kuusi");
-                        break;
-                        case 7:
-                        Console.WriteLine("seitsemän");
-                        break;
-                        case 8:
-                        Console.WriteLine("kahdeksan");
-                        break;
-                        case 9:
-                        Console.WriteLine("yhdeksän");
-                        break;
-                        case 10:
-                        Console.WriteLine("kymmenen");
-                        break;
-                    case 11:
-                        Console.WriteLine("yksitoista");
-                        break;
-                    case 12:
-                        Console.WriteLine("kaksitoista");
-                        break;
-                    case 13:
-                        Console.WriteLine("kolmetoista");
-                        break;
-                    case 14:
-                        Console.WriteLine("neljätoista");
-                        break;
-                    case 15:
-                        Console.WriteLine("viisitoista");
-                        break;
-                    case 16:
-                        Console.WriteLine("kuusitoista");
-                        break;
-                    case 17:
-                        Console.WriteLine("seitsemäntoista");
-                        break;
-                    case 18:
-                        Console.WriteLine("kahdeksantoista");
-                        break;
-                    case 19:
-                        Console.WriteLine("yhdeksäntoista");
-                        break;
-                    case 20:
-                        Console.WriteLine("kaksikymmentä");
-                        break;
-                    case 30:
-                        Console.WriteLine("kolmekymmentä");
-                        break;
-                    case 40:
-                        Console.WriteLine("neljäkymmentä");
-                        break;
-                    case 50:
-                        Console.WriteLine("viisikymmentä");
-                        break;
-                    case 60:
-                        Console.WriteLine("kuusikymmentä");
-                        break;
-                    case 70:
-                        Console.WriteLine("seitsemänkymmentä");
-                        break;
-                    case 80:
-                        Console.WriteLine("kahdeksankymmentä");
-                        break;
-                    case 90:
-                        Console.WriteLine("yhdeksänkymmentä");
-                        break;
-                    default:
-                        Console.WriteLine("Tarkista antamasi luku");
-                        break;
-                       } 
-                       if (nro < 20)
-                       {
-                         Console.WriteLine(nro);
-                       }
-                       if (nro.Length > 20)
-                       {
-                          Console.WriteLine(nro + "sataa")
-                       }*/
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("Pisteesi: " + pisteet);
+                    Console.WriteLine("Bonuspisteesi kerroin: 10");
+                    Console.WriteLine("Yhteensä: " + (pisteet * 10));
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("Pisteesi: " + pisteet);
+                    Console.WriteLine("Bonuspisteesi kerroin: 100");
+                    Console.WriteLine("Yhteensä: " + (pisteet * 100));
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine("Pisteesi: " + pisteet);
+                    Console.WriteLine("Bonuspisteesi kerroin: 1000");
+                    Console.WriteLine("Yhteensä: " + (pisteet * 1000));
+                    break;
+                default:
+                    Console.WriteLine("Tarkista syöttämäsi luku!");
+                    break;
             }
         }
-    
+
+        static void Main()
+        {
+
+            Console.WriteLine("Anna luku 0-999: ");
+            int numero = int.Parse(Console.ReadLine());
+            var ykkoset = new[] { "nolla", "yksi", "kaksi", "kolme", "neljä", "viisi", "kuusi", "seitsemän", "kahdeksan", "yhdeksän", "kymmenen", "yksitoista", "kaksitoista", "kolmetoista", "neljätoista", "viisitoista", "kuusitoista", "seitsemäntoista", "kahdeksantoista", "yhdeksäntoista" };
+            var kymmenet = new[] { "nolla", "kymmenen", "kaksikymmentä", "kolmekymmentä", "neljäkymmentä", "viisikymmentä", "kuusikymmentä", "seitsemänkymmentä", "kahdeksankymmentä", "yhdeksänkymmentä" };
+            if (numero == 0)
+                {
+                Console.WriteLine("nolla");
+                }
+                string sanat = "";
+                if ((numero / 100) > 0)
+                {
+                sanat += ykkoset[numero / 100] + "sataa ";
+                numero %= 100;
+                }
+                if (numero > 0)
+                {
+                if (numero < 20)
+                sanat += ykkoset[numero];
+                else
+                {
+                sanat += kymmenet[numero / 10];
+                 if ((numero % 10) > 0)
+                  sanat += " " + ykkoset[numero % 10];
+                }
+                }
+                Console.WriteLine(sanat);
+            }
+        }
+    }
+
 
         
     
