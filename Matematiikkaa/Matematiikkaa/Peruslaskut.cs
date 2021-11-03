02daf01420d50e15509997713b6646013efcbff4
unknown <sanna.kunnasluoto@edu.keuda.fi>
2021-11-03 10:52:59 +0200

    matikka

---
+++ Matematiikkaa/Matematiikkaa/Peruslaskut.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematiikkaa
+{
+    class Peruslaskut
+    {
+        int summa;
+        int erotus;
+        int osamaara;
+        int tulo;
+
+        public int Summa(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Erotus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Osamäärä(int a, int b)
+        {
+            return a / (double)b;
+        }
+
+        public int Tulo(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+
---
+++ Matematiikkaa/Matematiikkaa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Matematiikkaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Peruslaskut laskut = new Peruslaskut();
+            int s = laskut.Summa(4, 5);
+            int e = laskut.Erotus(5, 4);
+            double o = laskut.Osamäärä(4, 5);
+            int t = laskut.Tulo(5, 4);
+            Console.WriteLine("Lukujen {0} ja {1} summa, erotus, osamäärä ja tulo ovat {2}, {3}, {4} ja {5}", 4, 5, s, e, o, t);*/
+            Pankkitili sanna = new Pankkitili("Sanna", "Kunnasluoto", "123456-1234", 100.10);
+            Console.WriteLine(sanna.NaytaSaldo());
+            sanna.Pano(200);
+            Console.WriteLine(sanna.NaytaSaldo());
+            sanna.Otto(50);
+            Console.WriteLine(sanna.NaytaSaldo());
+        }
+    }
+}
