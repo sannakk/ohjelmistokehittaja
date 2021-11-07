0437d13a0d0d86a863075ac12d61bd3fac6d96b8
Sanna Kunnasluoto <sanna.kunnasluoto@hotmail.com>
2021-11-07 19:03:37 +0200

    olio-tehtävä

---
+++ Kirjasto/Kirjasto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kirjasto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiedot kirja1 = new Tiedot("Taikurin hattu", 150, "Tove Jansson", 2);
+            Tiedot kirja2 = new Tiedot("Puhdistus", 380, "Sofi Oksanen", 0);
+            Tiedot kirja3 = new Tiedot("Seitsemän veljestä", 405, "Aleksis Kivi", 1);
+            Console.WriteLine("Hei! Tervetuloa kirjastoon. Valitse haluamasi kirja, 1 Taikurin hattu, 2 Puhdistus, 3 Seitsemän veljestä: ");
+            
+        }
+    }
+}
---
+++ Kirjasto/Kirjasto/Tiedot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirjasto
+{
+    class Tiedot
+    {
+        string kirjanNimi;
+        int sivuMaara;
+        string kirjailija;
+        int kirjaKpl;
+
+        public Tiedot(string kirja, int sivut, string tekija, int maara)
+        {
+            kirjanNimi = kirja;
+            sivuMaara = sivut;
+            kirjailija = tekija;
+            kirjaKpl = maara;
+        }
+
+        public int KirjojenMaarat()
+        {
+            return kirjaKpl;
+        }
+        public void Lainaa(int kpl)
+        {
+            if(kpl > kirjaKpl)
+            {
+                Console.WriteLine("Et voi lainata näin montaa kirjaa!");
+            }
+            else
+            {
+                kirjaKpl = kirjaKpl - kpl;  
+            }
+            Console.WriteLine("Lainasit kirjoja " + kpl + " kpl, "  +  "kirjoja tällä hetkellä jäljellä " + kirjaKpl + " kpl.");
+        }
+
+        public void LisaaKappale(int kpl)
+        {
+            kirjaKpl = kirjaKpl + kpl;
+            Console.WriteLine("Kirjoja tällä hetkellä: " + kirjaKpl);
+        }
+    }
+}
