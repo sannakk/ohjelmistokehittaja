30d7deb56291adad8daf8637be5ed066daecb754
Sanna Kunnasluoto <sanna.kunnasluoto@hotmail.com>
2021-11-15 14:56:28 +0200

    olio-tehtävään lisäyksiä

--- Kirjasto/Kirjasto/Program.cs
+++ Kirjasto/Kirjasto/Program.cs
@@ -7,34 +7,43 @@ namespace Kirjasto
         static void Main(string[] args)
         {
             Tiedot kirja1 = new Tiedot("Taikurin hattu", 150, "Tove Jansson", 2);
-            Tiedot kirja2 = new Tiedot("Puhdistus", 380, "Sofi Oksanen", 0);
-            Tiedot kirja3 = new Tiedot("Seitsemän veljestä", 405, "Aleksis Kivi", 1);
-            Console.WriteLine("Hei "  + "! Tervetuloa kirjastoon. Valitse haluamasi kirja, 1 Taikurin hattu, 2 Puhdistus, 3 Seitsemän veljestä: ");
+            Tiedot kirja2 = new Tiedot("Puhdistus", 380, "Sofi Oksanen", 1);
+            Tiedot kirja3 = new Tiedot("Seitsemän veljestä", 405, "Aleksis Kivi", 3);
+            Console.WriteLine("Tervetuloa kirjastoon!");
+            Console.WriteLine("Valitse: ");
+            Console.WriteLine("1. " + kirja1.Teos());
+            Console.WriteLine("2. " + kirja2.Teos());
+            Console.WriteLine("3. " + kirja3.Teos());
             int valinta = int.Parse(Console.ReadLine());
             switch (valinta)
             {
                 case 1:
-                    Console.WriteLine(kirja1.kirjanNimi + "," + kirja1.sivuMaara + "sivua, " + kirja1.kirjailija + "." + " Kirjoja tällä hetkellä " + kirja1.kirjaKpl + "kpl.");
+                    Console.WriteLine("Kirja: " + kirja1.Teos());
+                    Console.WriteLine("Sivumäärä: " + kirja1.Sivut());
+                    Console.WriteLine("Kirjailija: " + kirja1.Kirjoittaja());
+                    Console.WriteLine("Kirjojen määrä: " + kirja1.KirjojenMaarat() + "kpl");
+                    kirja1.PalautaLainaa();
                     break;
                 case 2:
-                    Console.WriteLine(kirja2.kirjanNimi);
+                    Console.WriteLine("Kirja: " + kirja2.Teos());
+                    Console.WriteLine("Sivumäärä: " + kirja2.Sivut());
+                    Console.WriteLine("Kirjailija: " + kirja2.Kirjoittaja());
+                    Console.WriteLine("Kirjojen määrä: " + kirja2.KirjojenMaarat() + "kpl");
+                    kirja2.PalautaLainaa();
                     break;
                 case 3:
-                    Console.WriteLine(kirja3.kirjanNimi);
+                    Console.WriteLine("Kirja: " + kirja3.Teos());
+                    Console.WriteLine("Sivumäärä: " + kirja3.Sivut());
+                    Console.WriteLine("Kirjailija: " + kirja3.Kirjoittaja());
+                    Console.WriteLine("Kirjojen määrä: " + kirja3.KirjojenMaarat() + "kpl");
+                    kirja3.PalautaLainaa();
                     break;
                 default:
-                    Console.WriteLine("Antamallesi numerolle ei löydy kirjaa!");
+                    Console.WriteLine("Tarkista antamasi numero!");
                     break;
+            }                   
             }
-            Console.WriteLine("1 Lainaa kirja, 2 Palauta kirja, 3 Takaisin");
-            int valinta2 = int.Parse(Console.ReadLine());
-            switch (valinta2)
-            {
-                case 1:
-                    Tiedot.Lainaa(int);
-                    Console.WriteLine();
-                    break;
-            }
+            
         }
     }
-}
+
--- Kirjasto/Kirjasto/Tiedot.cs
+++ Kirjasto/Kirjasto/Tiedot.cs
@@ -6,10 +6,12 @@ namespace Kirjasto
 {
     class Tiedot
     {
-        public string kirjanNimi;
-        public int sivuMaara;
-        public string kirjailija;
-        public int kirjaKpl;
+        string kirjanNimi;
+        int sivuMaara;
+        string kirjailija;
+        int kirjaKpl;
+        int kpl;
+        int kpl2;
 
         public Tiedot(string kirja, int sivut, string tekija, int maara)
         {
@@ -19,32 +21,91 @@ namespace Kirjasto
             kirjaKpl = maara;
         }
 
+        public string Teos()
+        {
+            return kirjanNimi;
+        }
         public int KirjojenMaarat()
         {
             return kirjaKpl;
         }
         public void Lainaa(int kpl)
         {
+            Console.WriteLine("Monta haluat lainata? ");
+            kpl = int.Parse(Console.ReadLine());
+
             if (kpl > kirjaKpl)
             {
                 Console.WriteLine("Et voi lainata näin montaa kirjaa!");
+                Takaisin();
             }
             else
             {
                 kirjaKpl = kirjaKpl - kpl;
+                Console.WriteLine("Lainasit kirjoja " + kpl + " kpl, " + "kirjoja tällä hetkellä jäljellä " + kirjaKpl + " kpl.");
+                Takaisin();
             }
-            Console.WriteLine("Lainasit kirjoja " + kpl + " kpl, " + "kirjoja tällä hetkellä jäljellä " + kirjaKpl + " kpl.");
+
         }
 
-        public void LisaaKappale(int kpl)
+        public void Palauta(int kpl2)
         {
-            kirjaKpl = kirjaKpl + kpl;
-            Console.WriteLine("Kirjoja tällä hetkellä: " + kirjaKpl);
+            Console.WriteLine("Monta haluat palauttaa? ");
+            kpl2 = int.Parse(Console.ReadLine());
+
+            if (kpl2 >= kirjaKpl)
+            {
+                Console.WriteLine("Et voi palauttaa kirjaa, kaikki kappaleet ovat kirjastossamme!");
+                Takaisin();
+            }
+            else
+            {
+                kirjaKpl = kirjaKpl + kpl2;
+                Console.WriteLine("Kirjoja tällä hetkellä: " + kirjaKpl);
+                Takaisin();
+            }
         }
 
         public int Sivut()
         {
             return sivuMaara;
         }
+
+        public string Kirjoittaja()
+        {
+            return kirjailija;
+        }
+
+
+        public void PalautaLainaa()
+        {   
+            Console.WriteLine("1. Lainaa kirja");
+            Console.WriteLine("2. Palauta kirja");
+            int valinta2 = int.Parse(Console.ReadLine());
+            switch (valinta2)
+            {
+                case 1:
+                    Lainaa(kpl);
+                    break;
+                case 2:
+                    Palauta(kpl2);
+                    break;
+                default:
+                    Console.WriteLine("Tarkista antamasi numero!");
+                    break;
+            }
+        }
+
+        public void Takaisin()
+        {
+            Console.WriteLine("Palaa takaisin? Kyllä (k)");
+            string takas = Console.ReadLine();
+            if (takas == "k")
+            {
+                (jotain);
+            }
+        }
     }
 }
+
+
